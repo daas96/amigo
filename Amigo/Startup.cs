e8d040c6d777817c16f7fc8d0372597951caf2d2
sbennani <sbennani@DIM-P46600-01>
2019-05-23 11:42:32 -0400

    Ajoutez des fichiers projet.

---
+++ Amigo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Amigo.Startup))]
+namespace Amigo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
