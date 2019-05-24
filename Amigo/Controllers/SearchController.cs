4a45fad557eee4e7c84cc8de1efc3b67140747f5
sbennani <sbennani@DIM-P46600-01>
2019-05-24 19:13:30 -0400

    premiere version de la recherche

---
+++ Amigo/Controllers/SearchController.cs
@@ -0,0 +1,23 @@
+﻿using Amigo.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Amigo.Controllers
+{
+    public class SearchController : Controller
+    {
+        Database1Entities db = new Database1Entities();
+
+        // GET: /Search/Search
+        public ActionResult Index(string dep, string arr)
+        {
+
+            return View(db.travel.Where(x=> x.departure.Equals(dep) && x.arrival.Equals(arr)).ToList());
+        }
+    }
+}
---
+++ SQLQuery2.sql
@@ -0,0 +1,3 @@
+﻿
+INSERT INTO travel(Id,driver,departure,arrival,hour,date,smoke,animal,luggage,nbpassengers,nbpassengersmax)
+VALUES(1,'saad','chicout','mont',2,'2019-08-01',0,0,0,2,5);
