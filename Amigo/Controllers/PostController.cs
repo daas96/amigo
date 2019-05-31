fc044ada8970aae72855ade5d0954cdb8c7d569c
AnissaBokhamy <anissa.bokhamy@gmail.com>
2019-05-31 19:18:57 -0400

    Ajout trajet + sign up fonctionnel

--- Amigo/Controllers/PostController.cs
+++ Amigo/Controllers/PostController.cs
@@ -10,7 +10,7 @@ namespace Amigo.Controllers
     public class PostController : Controller
     {
         //Database1Entities db = new Database1Entities();
-        travel trip = new travel();
+        private travel trip { get; set; }
 
         [HttpGet]
         public ActionResult Index()
@@ -21,23 +21,29 @@ namespace Amigo.Controllers
         [HttpPost]
         public ActionResult Index(travel model)
         {
+            this.trip = new travel();
             if (ModelState.IsValid)
             {
                 int driverId = -1;
-                trip = model;
+                this.trip = model;
+                this.trip.Id = 16;
+                Console.WriteLine("COUCOU" + Session["SessionID"]);
                 if (Session["SessionID"] != null && Int32.TryParse(Session["SessionID"].ToString(), out driverId))
                 {
-                    model.driver = driverId;
+                    this.trip.driver = driverId;
+                    SaveTripInSession();
                     using (Database1Entities2 db = new Database1Entities2())
                     {
-                        db.travel.Add(model);
+                        db.travel.Add(this.trip);
                         db.SaveChanges();
                     }
                     ModelState.Clear();
-                    ViewBag.message = "Your trip from " + model.departure + " has been successfully posted !";
+                    ViewBag.message = "Your trip from " + this.trip.departure + " has been successfully posted !";
+                    return RedirectToAction("TripPosted");
                 }
                 else
                 {
+                    SaveTripInSession();
                     return RedirectToAction("LoginOrSignUp");
                 }
             }
@@ -90,12 +96,49 @@ namespace Amigo.Controllers
             return View();
         }
 
+        [HttpPost]
+        public ActionResult SignUp(users user)
+        {
+            if (ModelState.IsValid)
+            {
+                int driverId = -1;
+                using (Database1Entities2 db = new Database1Entities2())
+                    {
+                        db.users.Add(user);
+                        db.SaveChanges();
+
+                    Session["SessionID"] = user.Id;
+                    Session["FirstName"] = user.firstname;
+
+                    GetTripFromSession();
+                    if (Int32.TryParse(Session["SessionID"].ToString(), out driverId))
+                    {
+                        trip.driver = driverId;
+                        SaveTripInSession();
+                        return RedirectToAction("TripPosted");
+                    }
+                    ViewBag.Message = " Failed to Post your trip";
+                    return View();
+                }
+
+                    
+            }
+            return View();
+        }
+
 
         [HttpGet]
         public ActionResult TripPosted()
         {
-            ViewBag.Message = "Congratulations " + Session["FirstName"] + " for posting a trip from " + trip.departure + " to " + trip.arrival + "!";
-            return View();
+            GetTripFromSession();
+            using (Database1Entities2 db = new Database1Entities2())
+            {
+                db.travel.Add(trip);
+                db.SaveChanges();
+                ViewBag.Message = "Congratulations " + Session["FirstName"] + " for posting a trip from " + trip.departure + " to " + trip.arrival + "!";
+                DeleteTripInSession();
+                return View();
+            }
         }
 
 
@@ -144,8 +187,65 @@ namespace Amigo.Controllers
             }
         }
 
+        public void SaveTripInSession()
+        {
+            if(this.trip != null)
+            {
+                Session["TripId"] = this.trip.Id;
+                Session["TripDeparture"] = this.trip.departure;
+                Session["TripArrival"] = this.trip.arrival;
+                Session["TripDate"] = this.trip.date;
+                Session["TripHour"] = this.trip.hour;
+                Session["TripMaxPassengers"] = this.trip.nbpassengersmax;
+                Session["TripDriver"] = this.trip.driver;
+            }
+        }
 
- 
+        public void GetTripFromSession()
+        {
+            this.trip = new travel();
+            int tripId = -1;
+            if (Int32.TryParse(Session["TripId"].ToString(), out tripId))
+            {
+                this.trip.Id = tripId;
+
+                if (Session["TripDeparture"] != null && Session["TripArrival"] != null && Session["TripDate"] != null && Session["TripHour"] != null && Session["TripMaxPassengers"] != null && Session["TripDriver"] != null)
+                {
+                    int tripMaxPassengers = 0;
+                    int tripDriverId = -1;
+                    this.trip.departure = Session["TripDeparture"].ToString();
+                    this.trip.arrival = Session["TripArrival"].ToString();
+                    this.trip.departure = Session["TripDeparture"].ToString();
+                    this.trip.nbpassengers = 0;
+                    if (Int32.TryParse(Session["TripMaxPassengers"].ToString(), out tripMaxPassengers) && Int32.TryParse(Session["TripDriver"].ToString(), out tripDriverId))
+                    {
+                        this.trip.nbpassengersmax = tripMaxPassengers;
+                        this.trip.driver = tripDriverId;
+                    }
+                    else
+                    {
+                        this.trip = null;
+                    }
+                }
+                else
+                {
+                    this.trip = null;
+                }
+            }
+        }
+
+        public void DeleteTripInSession()
+        {
+            Session["TripId"] = null;
+            Session["TripDeparture"] = null;
+            Session["TripArrival"] = null;
+            Session["TripDate"] = null;
+            Session["TripHour"] = null;
+            Session["TripMaxPassengers"] = null;
+            Session["TripDriver"] = null;
+        }
+
+
     }
 
 
