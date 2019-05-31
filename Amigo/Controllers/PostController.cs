ade716c1cdcd4abf6887184ef5e01945e4ec7286
AnissaBokhamy <anissa.bokhamy@gmail.com>
2019-05-30 20:19:27 -0400

    Ajout des fenetres de connexion apres post d'un trajet, a peaufiner

--- Amigo/Controllers/PostController.cs
+++ Amigo/Controllers/PostController.cs
@@ -10,6 +10,7 @@ namespace Amigo.Controllers
     public class PostController : Controller
     {
         //Database1Entities db = new Database1Entities();
+        travel trip = new travel();
 
         [HttpGet]
         public ActionResult Index()
@@ -22,22 +23,81 @@ namespace Amigo.Controllers
         {
             if (ModelState.IsValid)
             {
-                using (Database1Entities2 db = new Database1Entities2())
+                int driverId = -1;
+                trip = model;
+                if (Session["SessionID"] != null && Int32.TryParse(Session["SessionID"].ToString(), out driverId))
                 {
-                    model.Id = 1;
-                    db.travel.Add(model);
-                    db.SaveChanges();
+                    model.driver = driverId;
+                    using (Database1Entities2 db = new Database1Entities2())
+                    {
+                        db.travel.Add(model);
+                        db.SaveChanges();
+                    }
+                    ModelState.Clear();
+                    ViewBag.message = "Your trip from " + model.departure + " has been successfully posted !";
                 }
-                ModelState.Clear();
-                ViewBag.message = "Your trip from " + model.departure + " has been successfully posted !";
+                else
+                {
+                    return RedirectToAction("LoginOrSignUp");
+                }
             }
             return View();
+
         }
 
         public ActionResult Details(int id)
         {
             return View ();
         }
+
+        [HttpGet]
+        public ActionResult LoginOrSignUp()
+        {
+            return View();
+        }
+
+        //         [HttpPost]
+        //         public ActionResult LoginOrSignUp(users model)
+        //         {
+        // 
+        //             return View();
+        //         }
+
+
+        [HttpPost]
+        public ActionResult LoginOrSignUp(users user)
+        {
+            using (Database1Entities2 db = new Database1Entities2())
+            {
+                var usr = db.users.Single(u => u.email.Equals(user.email) && u.password.Equals(user.password));
+                if (usr != null)
+                {
+                    Session["SessionID"] = usr.Id.ToString();
+                    Session["FirstName"] = usr.firstname.ToString();
+                    return RedirectToAction("TripPosted");
+                }
+                else
+                {
+                    ViewBag.Message = "Username or password is wrong";
+                }
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        public ActionResult TripPosted()
+        {
+            ViewBag.Message = "Congratulations " + Session["FirstName"] + " for posting a trip from " + trip.departure + " to " + trip.arrival + "!";
+            return View();
+        }
+
 
         public ActionResult Create()
         {
