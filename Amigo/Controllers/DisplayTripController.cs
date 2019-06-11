941e6fb585c409e07f0ad9df29f882a0ef0972dc
AnissaBokhamy <anissa.bokhamy@gmail.com>
2019-06-11 15:32:57 -0400

    Ajout page affichage trip a completer

---
+++ Amigo/Controllers/DisplayTripController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Amigo.Controllers
+{
+    public class DisplayTripController : Controller
+    {
+        // GET: DisplayTrip
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
